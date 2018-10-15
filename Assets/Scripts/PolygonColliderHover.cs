1ef19cf6eb5c9144469c44c375b5145a5a52f912
mgusev <vinsmain@gmail.com>
2018-10-15 12:38:53 +0500

    v.0.1 Добавлен фон, клик по конюшне

---
+++ Assets/Scripts/PolygonColliderHover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PolygonColliderHover : MonoBehaviour {
+
+	// Use this for initialization
+	private SpriteRenderer spriteRenderer;
+
+	void Start() {
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	void OnMouseOver() {
+		spriteRenderer.color = Color.red;
+	}
+
+	void OnMouseExit() {
+		spriteRenderer.color = Color.white;
+	}
+}
